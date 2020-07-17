4b8a125a7a12126b585c2323bc537f1ab6f8762f
Faiz <faiz-uz.zaman@dur.ac.uk>
2020-07-17 14:47:03 +0100

    Halfway through assigning meaning to imputed columns

--- faiz/manual_imputation/imputation.py
+++ faiz/manual_imputation/imputation.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from datetime import date, timedelta
 
+# reading in and assigning values
 data = pd.read_csv('isaFull.tsv', '\t')
 data['Visit date [EUPATH_0000091]'] = pd.to_datetime(data['Visit date [EUPATH_0000091]'])
 
@@ -20,11 +21,6 @@ series_end_date = minmax_time[1]
 patient_start_date = minmax_time_patient[0]
 patient_end_date = minmax_time_patient[1]
 
-anorexia = data[data["Anorexia [SYMP_0000523]"] == "Unable to assess"]
-
-age_temp_patient = patient[["Observation_Id", "Age at visit (years) [EUPATH_0000113]",\
-                            "Temperature (C) [EUPATH_0000110]", "Visit date [EUPATH_0000091]"]]
-
 '''Imputing backwards to series start date'''
 delta = timedelta(days=1)
 day_difference = (patient_start_date - series_start_date).days
@@ -32,14 +28,6 @@ day_difference = (patient_start_date - series_start_date).days
 # observation ID
 observation_id = patient.iloc[0]['Observation_Id']
 imputed_obs_id = observation_id - day_difference
-
-# participant ID and household ID stay the same
-# abdominal pain - 'unable to assess', or yes if from future visit
-# abdominal pain duration - 0 if unable to assess, compute from future visit duration otherwise
-# admitting hospital - na
-# age imputation below
-# anorexia - 'unable to assess', or yes if from future visit
-# 
 
 age = patient.iloc[0]["Age at visit (years) [EUPATH_0000113]"]
 
@@ -49,3 +37,9 @@ series_start_age = age - day_difference/365
 while series_start_date < patient_start_date:
     series_start_date += delta
     series_start_age += 1/365
+
+column = "Malaria diagnosis [EUPATH_0000090]"
+diagnosis = data[["Observation_Id", "Participant_Id", column, 'Visit date [EUPATH_0000091]']].dropna()
+print(diagnosis)
+print(diagnosis[diagnosis[column] == 'Unable to assess'])
+print(patient[["Observation_Id", "Participant_Id", column, 'Visit date [EUPATH_0000091]']])
