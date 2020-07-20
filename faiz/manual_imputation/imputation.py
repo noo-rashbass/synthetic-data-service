05f7561273ebfc4f36d597b939917622ed18ce12
Faiz <faiz-uz.zaman@dur.ac.uk>
2020-07-20 13:31:54 +0100

    Reorganising code to begin imputing

--- faiz/manual_imputation/imputation.py
+++ faiz/manual_imputation/imputation.py
@@ -5,43 +5,57 @@ from datetime import date, timedelta
 data = pd.read_csv('isaFull.tsv', '\t')
 data['Visit date [EUPATH_0000091]'] = pd.to_datetime(data['Visit date [EUPATH_0000091]'])
 
-patient = data[data['Participant_Id'] == 1001].\
-                        sort_values(['Visit date [EUPATH_0000091]']).reset_index(drop=True)
-
 minmax_time = [min(data['Visit date [EUPATH_0000091]']),\
                max(data['Visit date [EUPATH_0000091]'])]
 
-minmax_time_patient = [min(patient['Visit date [EUPATH_0000091]']),\
-                        max(patient['Visit date [EUPATH_0000091]'])]
-
-# earliest start date and latest end date
-series_start_date = minmax_time[0]
+# latest end date
 series_end_date = minmax_time[1]
 
-patient_start_date = minmax_time_patient[0]
-patient_end_date = minmax_time_patient[1]
-
-'''Imputing backwards to series start date'''
 delta = timedelta(days=1)
-day_difference = (patient_start_date - series_start_date).days
+patients = data['Participant_Id'].unique()
 
-# observation ID
-observation_id = patient.iloc[0]['Observation_Id']
-imputed_obs_id = observation_id - day_difference
+# imputing for each patient
+for patient_id in patients:
 
-age = patient.iloc[0]["Age at visit (years) [EUPATH_0000113]"]
+    patient = data[data['Participant_Id'] == patient_id].\
+                        sort_values(['Visit date [EUPATH_0000091]']).reset_index(drop=True)
 
-series_start_age = age - day_difference/365
+    # earliest start date
+    series_start_date = minmax_time[0]
+    
+    minmax_time_patient = [min(patient['Visit date [EUPATH_0000091]']),\
+                            max(patient['Visit date [EUPATH_0000091]'])]
+    
+    patient_start_date = minmax_time_patient[0]
+    patient_end_date = minmax_time_patient[1]
 
-# imputing from earliest start data in dataset to first patient visit
-while series_start_date < patient_start_date:
-    series_start_date += delta
-    series_start_age += 1/365
+    day_difference = (patient_start_date - series_start_date).days
 
-column = "Days since enrollment [EUPATH_0000191]"
+    observation_id = patient.iloc[0]['Observation_Id']
+    imputed_obs_id = observation_id - day_difference
+    age = patient.iloc[0]["Age at visit (years) [EUPATH_0000113]"]
+    series_start_age = age - day_difference/365
+
+    # imputing from series start date to patient start date
+    while series_start_date < patient_start_date:
+        
+        
+        
+        
+        
+        
+        
+        series_start_date += delta
+        series_start_age += 1/365
+
+    break
+
+'''
+column = "Hemoglobin (g/dL) [EUPATH_0000047]"
 diagnosis = data[["Observation_Id", "Participant_Id", column, 'Visit date [EUPATH_0000091]']]
 print(diagnosis)
 print(patient[["Observation_Id", "Participant_Id", column, 'Visit date [EUPATH_0000091]']])
 
 print(diagnosis[diagnosis[column].isnull()])
 print(diagnosis[column].unique())
+'''
