e1211cfe8fc9e685da20f32bb117be3bb5016e03
Faiz <faiz-uz.zaman@dur.ac.uk>
2020-07-23 15:40:21 +0100

    Finished 3rd round of imputation and fixed height bug - added files for first patient imputation

--- faiz/manual_imputation/imputation.py
+++ faiz/manual_imputation/imputation.py
@@ -131,6 +131,16 @@ for patient_id in patients:
     current_date = series_start_date
     same_height_throughout = is_equal_throughout(patient[height_string])
 
+    # averaging values
+    average_haemoglobin = round(patient[haemoglobin_string].mean(), 1)
+    average_temperature = round(patient[temperature_string].mean(), 1)
+    average_weight = round(patient[weight_string].mean() * 2) / 2
+
+    # set height as NAN for children and constant for adults (may change for children in the future)
+    height = np.nan
+    if same_height_throughout:
+        height = first_row[height_string]
+
     # imputing from series start date to patient start date
     while current_date < patient_start_date:
 
@@ -155,14 +165,6 @@ for patient_id in patients:
         headache_duration = first_row['Headache duration (days) [EUPATH_0000159]']
         headache, headache_duration = duration_calculator(headache_duration, day_difference)
 
-        # set height as NAN for children and constant for adults (may change for children in the future)
-        height = np.nan
-        if same_height_throughout:
-            height = first_row[height_string]
-
-        # averaging haemoglobin
-        haemoglobin = round(patient[haemoglobin_string].mean(), 1)
-
         jaundice_duration = first_row['Jaundice duration (days) [EUPATH_0000160]']
         jaundice, jaundice_duration = duration_calculator(jaundice_duration, day_difference)
 
@@ -175,16 +177,13 @@ for patient_id in patients:
         seizures_duration = first_row['Seizures duration (days) [EUPATH_0000163]']
         seizures, seizures_duration = duration_calculator(seizures_duration, day_difference)
 
-        temperature = round(patient[temperature_string].mean(), 1)
-        visit_date = current_date
-
         vomiting_duration = first_row['Seizures duration (days) [EUPATH_0000163]']
         vomiting, vomiting_duration = duration_calculator(vomiting_duration, day_difference)
 
         # weight NAN for children, averaging for adults
         weight = np.nan
         if current_age > 20:
-            weight = round(patient[weight_string].mean() * 2) / 2
+            weight = average_weight
 
         imputed_row = {
             "Observation_Id": imputed_obs_id, 
@@ -212,8 +211,8 @@ for patient_id in patients:
             "Fever, subjective duration (days) [EUPATH_0000164]": febrile_duration,
             "Headache [HP_0002315]": headache,
             "Headache duration (days) [EUPATH_0000159]": headache_duration,
-            "Height (cm) [EUPATH_0010075]": height,
-            "Hemoglobin (g/dL) [EUPATH_0000047]": haemoglobin,
+            "Height (cm) [EUPATH_0010075]": str(int(height)),
+            "Hemoglobin (g/dL) [EUPATH_0000047]": average_haemoglobin,
             "Hospital admission date [EUPATH_0000319]": hospital_admission_date,
             "Hospital discharge date [EUPATH_0000320]": hospital_discharge_date,
             "ITN last night [EUPATH_0000216]": itn,
@@ -235,8 +234,8 @@ for patient_id in patients:
             "Severe malaria criteria [EUPATH_0000046]": severe_malaria_criteria,
             "Subjective fever [EUPATH_0000100]": subjective_fever,
             "Submicroscopic Plasmodium present, by LAMP [EUPATH_0000487]": submic_plas_present,
-            "Temperature (C) [EUPATH_0000110]": temperature,
-            "Visit date [EUPATH_0000091]": visit_date,
+            "Temperature (C) [EUPATH_0000110]": average_temperature,
+            "Visit date [EUPATH_0000091]": current_date,
             "Visit type [EUPATH_0000311]": visit_type,
             "Vomiting [HP_0002013]": vomiting,
             "Vomiting duration (days) [EUPATH_0000165]": vomiting_duration,
@@ -349,7 +348,6 @@ for patient_id in patients:
             seizures, seizures_duration = duration_calculator(seizures_duration, day_difference)
 
             current_temperature = apply_linear_modelling(current_temperature, daily_temperature_change)
-            visit_date = current_date
 
             vomiting_duration = patient.iloc[real_visit]['Seizures duration (days) [EUPATH_0000163]']
             vomiting, vomiting_duration = duration_calculator(vomiting_duration, day_difference)
@@ -406,7 +404,7 @@ for patient_id in patients:
                 "Subjective fever [EUPATH_0000100]": subjective_fever,
                 "Submicroscopic Plasmodium present, by LAMP [EUPATH_0000487]": submic_plas_present,
                 "Temperature (C) [EUPATH_0000110]": round(current_temperature, 1),
-                "Visit date [EUPATH_0000091]": visit_date,
+                "Visit date [EUPATH_0000091]": current_date,
                 "Visit type [EUPATH_0000311]": visit_type,
                 "Vomiting [HP_0002013]": vomiting,
                 "Vomiting duration (days) [EUPATH_0000165]": vomiting_duration,
@@ -421,11 +419,6 @@ for patient_id in patients:
 
         observation_id += 1
         current_date += delta
-
-    # display results of first two rounds so far
-    #pd.set_option('display.max_rows', None)
-    #diagnosis = imputed_patient[["Observation_Id", weight_string, visit_date_string, 'real']]
-    #print(diagnosis.sort_values([visit_date_string]))
 
 # ================================================================================================
 
@@ -443,21 +436,87 @@ for patient_id in patients:
         febrile_duration = headache_duration = joints_pain_duration =\
         muscle_aches_duration = seizures_duration = vomiting_duration = 0
 
+    days_since_enrollment = np.nan
+
+    # set height as NAN for children and constant for adults (may change for children in the future)
+    height = np.nan
+    if same_height_throughout:
+        height = first_row[height_string]
+
     while current_date <= series_end_date:
 
-        current_date += delta
+        imputed_row = {
+            "Observation_Id": observation_id, 
+            "Participant_Id": patient_id,
+            "Household_Id": household_id,
+            "Abdominal pain [HP_0002027]": abd_pain,
+            "Abdominal pain duration (days) [EUPATH_0000154]": abd_pain_duration,
+            "Admitting hospital [EUPATH_0000318]": admitting_hospital,
+            "Age at visit (years) [EUPATH_0000113]": round(current_age, 2),
+            "Anorexia [SYMP_0000523]": anorexia,
+            "Anorexia duration (days) [EUPATH_0000155]": anorexia_duration,
+            "Asexual Plasmodium parasite density, by microscopy [EUPATH_0000092]": ax_plas_density,
+            "Asexual Plasmodium parasites present, by microscopy [EUPATH_0000048]": ax_plas_present,
+            "Basis of complicated diagnosis [EUPATH_0000316]": complex_diagnosis_basis,
+            "Complicated malaria [EUPATH_0000040]": complicated_malaria,
+            "Cough [SYMP_0000614]": cough,
+            "Cough duration (days) [EUPATH_0000156]": cough_duration,
+            "Days since enrollment [EUPATH_0000191]": days_since_enrollment,
+            "Diagnosis at hospitalization [EUPATH_0000638]": diagnosis_at_hospital,
+            "Diarrhea [DOID_13250]": diarrhoea,
+            "Diarrhea duration (days) [EUPATH_0000157]": diarrhoea_duration,
+            "Fatigue [SYMP_0019177]": fatigue,
+            "Fatigue duration (days) [EUPATH_0000158]": fatigue_duration,
+            "Febrile [EUPATH_0000097]": febrile,
+            "Fever, subjective duration (days) [EUPATH_0000164]": febrile_duration,
+            "Headache [HP_0002315]": headache,
+            "Headache duration (days) [EUPATH_0000159]": headache_duration,
+            "Height (cm) [EUPATH_0010075]": height,
+            "Hemoglobin (g/dL) [EUPATH_0000047]": average_haemoglobin,
+            "Hospital admission date [EUPATH_0000319]": hospital_admission_date,
+            "Hospital discharge date [EUPATH_0000320]": hospital_discharge_date,
+            "ITN last night [EUPATH_0000216]": itn,
+            "Jaundice [HP_0000952]": jaundice,
+            "Jaundice duration (days) [EUPATH_0000160]": jaundice_duration,
+            "Joint pains [SYMP_0000064]": joint_pains,
+            "Joint pains duration (days) [EUPATH_0000161]": joint_pains_duration,
+            "Malaria diagnosis [EUPATH_0000090]": malaria_diagnosis,
+            "Malaria diagnosis and parasite status [EUPATH_0000338]": malaria_diagnosis_parasite,
+            "Malaria treatment [EUPATH_0000740]": malaria_treatment,
+            "Muscle aches [EUPATH_0000252]": muscle_aches,
+            "Muscle aches duration (days) [EUPATH_0000162]": muscle_aches_duration,
+            "Non-malaria medication [EUPATH_0000059]": non_malaria_medication,
+            "Other diagnosis [EUPATH_0000317]": other_diagnosis,
+            "Other medical complaint [EUPATH_0020002]": other_medical_complaint,
+            "Plasmodium gametocytes present, by microscopy [EUPATH_0000207]": plas_gam_present,
+            "Seizures [SYMP_0000124]": seizures,
+            "Seizures duration (days) [EUPATH_0000163]": seizures_duration,
+            "Severe malaria criteria [EUPATH_0000046]": severe_malaria_criteria,
+            "Subjective fever [EUPATH_0000100]": subjective_fever,
+            "Submicroscopic Plasmodium present, by LAMP [EUPATH_0000487]": submic_plas_present,
+            "Temperature (C) [EUPATH_0000110]": average_temperature,
+            "Visit date [EUPATH_0000091]": current_date,
+            "Visit type [EUPATH_0000311]": visit_type,
+            "Vomiting [HP_0002013]": vomiting,
+            "Vomiting duration (days) [EUPATH_0000165]": vomiting_duration,
+            "Weight (kg) [EUPATH_0000732]": average_weight,
+            "real": False
+        }
 
+        # append imputed row to patient
+        imputed_patient = imputed_patient.append(imputed_row, ignore_index=True)
+        current_age += 1/365
+        observation_id += 1
+        current_date += delta
 
     break
 
+# display results of first two rounds so far
+pd.set_option('display.max_rows', None)
+diagnosis = imputed_patient[["Observation_Id", height_string, visit_date_string, 'real']]
+#print(diagnosis.sort_values([visit_date_string]))
+
+imputed_patient = imputed_patient.sort_values([visit_date_string])
+#imputed_patient.to_csv('first_patient_imputed.csv', index=False)
+
 print("--- %s seconds ---" % (time.time() - start_time))
-
-'''
-column = height_string
-diagnosis = data[["Observation_Id", "Participant_Id", column, visit_date_string]]
-print(diagnosis)
-print(patient[["Observation_Id", "Participant_Id", column, visit_date_string]])
-
-print(diagnosis[diagnosis[column].isnull()])
-print(diagnosis[column].unique())
-'''
