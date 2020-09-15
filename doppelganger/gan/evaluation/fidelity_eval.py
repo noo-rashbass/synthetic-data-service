52ec06b497beeca39797574880b3f73df013816b
Evonne <epcl2@cam.ac.uk>
2020-09-15 18:47:29 +0800

    refactor: Dict to loop through autocorr, separate py file

--- doppelganger/gan/evaluation/fidelity_eval.py
+++ doppelganger/gan/evaluation/fidelity_eval.py
@@ -1,9 +1,16 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+import tensorflow as tf
+
+from statsmodels.tsa.stattools import acf
+import datetime as dt
+import plotly.graph_objects as go
+
+from sklearn.model_selection import train_test_split
 from sklearn.manifold import TSNE
 from sklearn.decomposition import PCA
-import tensorflow as tf
-from sklearn.model_selection import train_test_split
+
 
 def visualization (ori_data, generated_data, analysis, syn_name):
     """Using PCA or tSNE for generated and original data visualization.
@@ -139,3 +146,101 @@ def discriminative_model(input_size, hidden_dim):
     model.compile(optimizer="adam", loss=tf.keras.losses.BinaryCrossentropy())
     
     return model
+
+
+def subset (df, col):
+    """
+    gets the rows in the dataframe that has values "yes" in specified column
+
+    Args:
+    df: dataframe 
+    col: column name in the dataframe where filtering should be done
+
+    Returns:
+    new_df: dataframe containing rows that have values "yes" in specified column
+    """
+    new_df= df[df[col].isin(["Yes","yes"])]
+    
+    return new_df
+
+
+def auto_prep (df):
+    """
+    accepts a dataframe that contains date of events as input
+    and computes the number of events that happen per week
+
+    Args:
+    df: dataframe with visit date
+
+    Returns:
+    df: dataframe with visit date 
+    table_freq: dataframe with number of events per week
+    """
+
+    # Remove empty days and transform visit date to date time data type
+    df = df.dropna(subset=['Visit date [EUPATH_0000091]'])
+    df['Visit date [EUPATH_0000091]'] = pd.to_datetime(df['Visit date [EUPATH_0000091]'])
+
+    # Create visit week column containing date on MONDAY of the week the visit took place in:
+    df['visit_week'] = df['Visit date [EUPATH_0000091]'].apply(lambda d: d-dt.timedelta(days=d.weekday()))
+    
+    # Create frequency table of event
+    table_freq = pd.value_counts(df['visit_week']).to_frame().reset_index()
+    table_freq = table_freq.set_index("index").sort_index()
+    
+    return df, table_freq
+
+
+def autocorrelation (df_ori, df_gen, table_freq_ori, table_freq_gen, syn_name, description):
+    """
+    plots autocorrelation for original and generate data
+
+    Args:
+    df_ori: dataframe of real data with dates
+    df_gen: dataframe of gen data with dates
+    table_freq_ori: dataframe with number of events per week for ori data, usually from auto_prep function
+    table_freq_gen: dataframe with number of events per week for gen data, usually from auto_prep function
+    syn_name: string format, version of synthetic data (for labelling)
+    description: string format, any extra information for title of plot (for labelling)
+
+    Returns:
+    acf_df: dataframe with autocorrelation values for real and gen data
+    fig: figure to be plotted
+    """
+    
+    #making sure the start and finish are the same and that weeks with no events are 0
+    weeks_df = pd.date_range(start=min(df_ori['visit_week'].min(), df_gen['visit_week'].min()),
+                            end=max(df_ori['visit_week'].max(),df_gen['visit_week'].max()), freq='W-MON')
+    weeks_df = pd.DataFrame(weeks_df)
+    weeks_df.columns = ['index']
+    weeks_df = weeks_df.set_index('index').sort_index()
+
+    table_freq_ori = pd.merge(weeks_df, table_freq_ori, on="index", how='outer').fillna(0)
+    table_freq_gen = pd.merge(weeks_df, table_freq_gen, on="index", how='outer').fillna(0)
+
+    #autocorrelation from statsmodels.tsa.stattools
+    acf_ori = acf(table_freq_ori['visit_week'], fft=False)
+    acf_gen = acf(table_freq_gen['visit_week'], fft=False)
+    
+    acf_ori_df = pd.DataFrame(acf_ori)
+    acf_gen_df = pd.DataFrame(acf_gen)
+    
+    acf_df = pd.concat([acf_ori_df, acf_gen_df], axis=1)
+    acf_df.columns = ["original","generated"]
+    
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(
+                    x=acf_df.index, 
+                    y=acf_df["original"],
+                    mode='lines+markers',
+                    name='original data'))
+    fig.add_trace(go.Scatter(
+                    x=acf_df.index, 
+                    y=acf_df["generated"],
+                    mode='lines+markers',
+                    name='generated data'))
+    fig.update_layout(xaxis_title="lag",
+                      yaxis_title="autocorrelation",
+                      title=syn_name + description)
+    
+    return acf_df, fig
