6e5830a6fbcbff0cbf5b2443adf99561bf512574
Evonne <epcl2@cam.ac.uk>
2020-09-25 16:00:48 +0800

    Attempt to disable file download jupyter

---
+++ doppelganger/gan/handlers.py
@@ -0,0 +1,16 @@
+# this file does not have anything to do with the gan
+# this is an attempt to disable file downloand in jupyter
+
+from tornado import web
+from notebook.base.handlers import IPythonHandler
+
+class ForbidFilesHandler(IPythonHandler):
+  @web.authenticated
+  def head(self, path):
+    self.log.info("HEAD: File download forbidden.")
+    raise web.HTTPError(403)
+
+  @web.authenticated
+  def get(self, path, include_body=True):
+    self.log.info("GET: File download forbidden.")
+    raise web.HTTPError(403)
